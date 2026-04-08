66e0b0d486645428ebbec5573e6f6dff24b27493
Lívia Farias <liviasousafarias@outlook.com>
2026-04-08 18:38:27 -0300

    chore: página inicial do projeto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Bem vindo(a) ao rastreador de despesas!");
+        Console.WriteLine("Qual o seu nome?");
+        string nomeUsuario = Console.ReadLine();
+        Console.WriteLine("Rastreador de despesas de " + nomeUsuario);
+
+    }
+}
