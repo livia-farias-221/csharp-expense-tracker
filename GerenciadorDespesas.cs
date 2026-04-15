5db7e0ff778d1ad553627928e8e320eca6627533
Lívia Farias <liviasousafarias@outlook.com>
2026-04-14 21:58:28 -0300

    feat: adicionando função de somar gastos por categoria, atualizando menu de opções de lista numerada pra lista com bullet points, atualizando dicionáio para receber uma lista ao invés de string para facilitar manipulação dos dados.

--- GerenciadorDespesas.cs
+++ GerenciadorDespesas.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 
 public class GerenciadorDespesas{
 
-    public void ManipularDespesas(Dictionary<string, List<string>> gastosDicionario){
+    public void ManipularDespesas(Dictionary<string, List<(string Nome, decimal Valor)>> gastosDicionario){
     
-    Console.WriteLine("O que deseja fazer agora?");
-    Console.WriteLine("1-Listar despesas por categoria\n 2-Excluir despesa\n 3-Excluir categoria\n 4-Editar nome de uma categoria\n 5-Editar valor de um gasto\n 6-Excluir um gasto");
+    
 
     string opcaoMenu = "";
+    string opcaoSomar = "";
     string selecionarCategoria = "";
     
     while (true)
 
     {
+        Console.WriteLine("O que deseja fazer agora?");
+        Console.WriteLine("1-Listar despesas por categoria\n2-Excluir despesa\n3-Excluir categoria\n4-Editar nome de uma categoria\n5-Editar valor de um gasto\n6-Excluir um gasto\n7-Encerrar");
         Console.WriteLine("Digite o número da opção: ");
         opcaoMenu = Console.ReadLine();
         
@@ -30,9 +32,38 @@ public class GerenciadorDespesas{
             
             foreach (var gasto in gastosDicionario[selecionarCategoria])
             {
-                Console.WriteLine(gasto);
+                Console.WriteLine($"{gasto.Nome} : R$ {gasto.Valor}");
             }
 
+                Console.WriteLine("Gostaria de soma as despesas dessa categoria? (S/N)");
+                opcaoSomar = Console.ReadLine();
+                opcaoSomar = opcaoSomar.ToUpper();
+
+                if (opcaoSomar == "S")
+                {
+                    decimal somarGastosCategoria = 0;
+                    foreach (var gasto in gastosDicionario[selecionarCategoria])
+                    {
+                        somarGastosCategoria += gasto.Valor;
+                    }
+
+                    Console.WriteLine($"Total da categoria {selecionarCategoria}: R$ {somarGastosCategoria}");
+                    continue;
+                }
+
+                else if (opcaoSomar == "N"){
+                    continue;
+                }
+
+                else{
+                    Console.WriteLine("Opção inválida. Digite novamente.");
+                    continue;
+                }
+
+        }
+
+        else if (opcaoMenu == "7"){
+            Console.WriteLine("Encerrando...");
             break;
         }
 
